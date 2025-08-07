02fd6dcefdab9832c3dea55e280c096af4214840
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-08-07 16:36:09 -0300

    Add files via upload

---
+++ firestore_utils.py
@@ -0,0 +1,22 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Inicializar o Firebase (reutiliza se já iniciado)
+if not firebase_admin._apps:
+    cred = credentials.Certificate('firebase-adminsdk.json')
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def salvar_aposta(uid, numeros, concurso):
+    doc_ref = db.collection('usuarios').document(uid).collection('apostas').document(str(concurso))
+    doc_ref.set({
+        'numeros': numeros,
+        'concurso': concurso,
+        'timestamp': firestore.SERVER_TIMESTAMP
+    })
+
+def obter_apostas(uid):
+    apostas_ref = db.collection('usuarios').document(uid).collection('apostas')
+    docs = apostas_ref.order_by('timestamp', direction=firestore.Query.DESCENDING).stream()
+    return [{**doc.to_dict(), 'id': doc.id} for doc in docs]
