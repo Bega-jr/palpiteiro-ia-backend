75bd8e184229ba12c38eb9e2c59af00c20d052aa
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-11-19 14:38:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ from flask_cors import CORS
 
 # Rotas
 from routes.estatisticas_route import estatisticas_bp
+from routes.apostas_route import apostas_bp
 
 app = Flask(__name__)
 CORS(app)
 
 
-# Rota padrão apenas para teste
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"status": "Backend Palpiteiro IA rodando!"})
 
 
-# Registrar as rotas
+# Registrar rotas
 app.register_blueprint(estatisticas_bp)
+app.register_blueprint(apostas_bp)
 
 
 if __name__ == "__main__":
-    # Necessário para rodar no GitHub Codespaces
     app.run(
         host="0.0.0.0",
         port=5000,
