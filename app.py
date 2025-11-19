cce4db3a11d3c3b363cf05e4737efa4603f89164
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-11-19 17:51:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
 
-# Rotas
 from routes.estatisticas_routes import estatisticas_bp
 from routes.apostas_routes import apostas_bp
 
 app = Flask(__name__)
 CORS(app)
 
-# Registrar blueprints com prefixo de URL
+# Registrar blueprints
 app.register_blueprint(estatisticas_bp, url_prefix='/estatisticas')
 app.register_blueprint(apostas_bp, url_prefix='/apostas')
 
-# Health Check (necessário para o Render)
 @app.route("/health")
 def health():
     return {"status": "ok"}, 200
 
-# Rota simples para teste local
 @app.route("/")
 def home():
     return {"message": "Palpiteiro IA Backend - OK!"}
