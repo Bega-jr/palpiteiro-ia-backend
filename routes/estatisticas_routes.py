707e64520b8d142aebe33bd48fa6057fa8b028e0
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-11-19 17:49:58 -0300

    Update estatisticas_routes.py

--- routes/estatisticas_routes.py
+++ routes/estatisticas_routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, jsonify
 from services.estatisticas_service import EstatisticasService
 
-estatisticas_bp = Blueprint("estatisticas_bp", __name__)
+# Nome simples do blueprint
+estatisticas_bp = Blueprint("estatisticas", __name__)
 service = EstatisticasService()
 
-@estatisticas_bp.route("/estatisticas", methods=["GET"])
+@estatisticas_bp.route("/", methods=["GET"])
 def estatisticas():
     resultado = service.gerar_estatisticas()
     return jsonify(resultado)
